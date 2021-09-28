b20b90f9d05af929855cf4d780a8af99a0d8cff5
Dipti104706 <diptimayeebehura104@gmail.com>
2021-09-28 12:28:38 +0530

    [Dipti] modified code to give player input and determining computer return for tictoctoe

--- TicTocToe/TicTocToe.cs
+++ TicTocToe/TicTocToe.cs
@@ -5,6 +5,17 @@ namespace TicTocToe
 {
     class TicTocToe
     {
+        public void CreatingBoard()
+        {
+            char[] array = new char[10];
+            int i;
+            for (i = 1; i == array.Length; i++)
+            {
+                array[i] = ' ';
+            }
+            Console.WriteLine("Array is", +array[i]);
+        }
+
         public char PlayerInput()
         {
             Console.WriteLine("Enter the character either X or O");
